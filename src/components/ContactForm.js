f473a684e1aa54360bab94bf8cd06a1f712801a5
DavidBoccalandro <davidboccalandro@hotmail.com>
2022-03-30 18:19:36 -0300

    testing form 2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -65,7 +65,7 @@ function ContactForm() {
 
 	return (
 		<ContactFormStyles>
-			<form name="contact" method="post" data-netlify="true">
+			<form name="contact" method="post">
 				<input type="hidden" name="form-name" value="contact" />
 				<div className="form-group">
 					<label htmlFor="name">
