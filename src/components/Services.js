08d735f0f1d130d1d0555320f04f555a78364f2d
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-22 16:45:29 -0300

    add icons into techs and tools skill section

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,5 @@
-import React from "react";
-import {
-	DiNodejsSmall,
-	DiPostgresql,
-} from "react-icons/di";
+import React from 'react';
+import { DiNodejsSmall, DiPostgresql, DiSass } from 'react-icons/di';
 import {
 	SiReact,
 	SiAngular,
@@ -11,30 +8,36 @@ import {
 	SiRedux,
 	SiSequelize,
 	SiTypescript,
-} from "react-icons/si";
-import { ImGit } from "react-icons/im";
-import styled from "styled-components";
-import SectionTitle from "./SectionTitle";
-import ServicesSectionItem from "./ServicesSectionItem";
+	SiHtml5,
+	SiCss3,
+	SiReactivex,
+	SiBootstrap,
+	SiJest,
+	SiTestinglibrary,
+	SiJasmine,
+} from 'react-icons/si';
+import { ImGit } from 'react-icons/im';
+import styled from 'styled-components';
+import SectionTitle from './SectionTitle';
+import ServicesSectionItem from './ServicesSectionItem';
+import ngRxSvg from '../assets/images/ngrx.svg';
+import angularMaterial from '../assets/images/angularMaterial.svg';
 
 const ServicesItemStyles = styled.div`
 	padding: 5rem 0;
 	.services__allItems {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		gap: 10rem;
-		justify-content: space-between;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 5rem;
+		justify-content: center;
 		margin-top: 5rem;
 	}
 
 	@media only screen and (max-width: 768px) {
 		.services__allItems {
-			grid-template-columns: repeat(3, 1fr);
-			justify-content: center;
-			max-width: 350px;
 			margin: 00 auto;
 			margin-top: 5rem;
-			gap: 2rem;
+			gap: 3rem 2rem;
 		}
 	}
 `;
@@ -45,24 +48,25 @@ function Services() {
 			<div className="container">
 				<SectionTitle heading="SKILLS" subheading="Technologies and tools" />
 				<div className="services__allItems">
-					<ServicesSectionItem
-						icon={<SiJavascript />}
-						description="JavaScript"
-					/>
-					<ServicesSectionItem
-						icon={<SiTypescript />}
-						description="TypeScript"
-					/>
 					<ServicesSectionItem icon={<SiAngular />} description="Angular" />
 					<ServicesSectionItem icon={<SiReact />} description="React" />
+					<ServicesSectionItem icon={<SiJavascript />} description="JavaScript" />
+					<ServicesSectionItem icon={<SiTypescript />} description="TypeScript" />
 					<ServicesSectionItem icon={<SiRedux />} description="Redux" />
+					<ServicesSectionItem icon={<img src={ngRxSvg} alt="ngRx icon" />} description="NgRx" />
+					<ServicesSectionItem icon={<SiReactivex />} description="RxJS" />
+					<ServicesSectionItem icon={<SiHtml5 />} description="HTML" />
+					<ServicesSectionItem icon={<SiCss3 />} description="CSS" />
+					<ServicesSectionItem icon={<DiSass />} description="Sass" />
+					<ServicesSectionItem icon={<img src={angularMaterial} alt="Angular Material icon" />} description="Angular Material" />
+					<ServicesSectionItem icon={<SiBootstrap />} description="Bootstrap" />
+					<ServicesSectionItem icon={<SiJest />} description="Jest" />
+					<ServicesSectionItem icon={<SiTestinglibrary />} description="React Testing Library" />
+					<ServicesSectionItem icon={<SiJasmine />} description="Jasmine" />
 					<ServicesSectionItem icon={<ImGit />} description="Git" />
 					<ServicesSectionItem icon={<DiNodejsSmall />} description="Node.js" />
 					<ServicesSectionItem icon={<SiExpress />} description="Express.js" />
-					<ServicesSectionItem
-						icon={<DiPostgresql />}
-						description="PostgreSQL"
-					/>
+					<ServicesSectionItem icon={<DiPostgresql />} description="PostgreSQL" />
 					<ServicesSectionItem icon={<SiSequelize />} description="Sequelize" />
 				</div>
 			</div>
--- src/components/ServicesSectionItem.js
+++ src/components/ServicesSectionItem.js
@@ -1,24 +1,38 @@
-import React from "react";
-import Description from "./Description";
-import styled from "styled-components";
+import React from 'react';
+import Description from './Description';
+import styled from 'styled-components';
 
 const ItemStyles = styled.div`
 	text-align: center;
+	width: 120px;
 	.servicesItem__icon {
-		svg {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		svg, img {
 			width: 6rem;
 		}
 	}
 	.servicesItem__title {
 		font-size: 2.5rem;
-		font-family: "Montserrat SemiBold";
+		font-family: 'Montserrat SemiBold';
 	}
 	.paragraph {
 		margin-top: 2rem;
 	}
+
+	@media only screen and (max-width: 768px) {
+		.servicesItem__title {
+			font-size: 1.5rem;
+		}
+		.paragraph {
+			margin-top: 1rem;
+		}
+	}
 `;
 
-function ServicesSectionItem({ icon = <></>, description = "Description" }) {
+function ServicesSectionItem({ icon = <></>, description = 'Description' }) {
 	return (
 		<ItemStyles>
 			<div className="servicesItem__icon">{icon}</div>
